14c23fcab25a7c4cd559c4f933022e7eff6ebfd0
Hue1601 <phamthihue3340@gmail.com>
2025-03-12 10:45:12 +0700

    Add validate password

--- shoes-shop-be/src/main/java/org/example/shoesshopbe/Exception/MessageError.java
+++ shoes-shop-be/src/main/java/org/example/shoesshopbe/Exception/MessageError.java
@@ -3,12 +3,13 @@ package org.example.shoesshopbe.Exception;
 public enum MessageError {
 
     INVALID_USERNAME("Invalid username or password."),
+    SUCCESSFUL("Successful"),
+    UNEXPECTED_ERROR("An unexpected error occurred"),
     REQUIRE("Vui lòng nhập đầy đủ thông tin");
 
     private String message;
 
     MessageError(String message) {
-
         this.message = message;
     }
 
--- shoes-shop-be/src/main/java/org/example/shoesshopbe/Model/Users.java
+++ shoes-shop-be/src/main/java/org/example/shoesshopbe/Model/Users.java
@@ -1,7 +1,7 @@
 package org.example.shoesshopbe.Model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.example.shoesshopbe.Exception.MessageError;
 
@@ -15,15 +15,23 @@ import java.time.LocalDateTime;
 public class Users {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotBlank(message = "Vui lòng nhập đầy đủ thông tin")
     private String username;
+
     @NotBlank(message = "Vui lòng nhập đầy đủ thông tin")
+    @Size(min = 6, message = "Mật khẩu ít nhất là 6 ký tự")
     private String password;
+
     @Column(name="PhoneNumber")
     @NotBlank(message = "Vui lòng nhập đầy đủ thông tin")
+    @Pattern(regexp = "\\d{10}", message = "Số điện thoại chỉ được chứa số và phải có đúng 10 chữ số")
     private String phoneNumber;
+
     @NotBlank(message = "Vui lòng nhập đầy đủ thông tin")
+    @Email(message="Vui lòng nhập đúng định dạng email")
     private String email;
+
     @Column(name="GoogleLogin")
     private boolean googleLogin;
     @Column(name = "CreatedAt")
